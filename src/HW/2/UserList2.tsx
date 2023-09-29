c3227442ea7cc6aeb34d3b634a1df7665af749cf
ula28 <ula24499@gmail.com>
2023-09-30 00:54:42 +0200

    d2

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -5,12 +5,12 @@ import {UsersObjectType, UserType} from './HW2';
 export type UserList2PropsType = {
 	users: UsersObjectType; // пропиши типизацию
 	filterUsers: ()=>void;// пропиши  типизацию
-	
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
 	return (
 		<div id={'hw02-users'}>
+			
 			<h2>User List:</h2>
 
 			<button id={'hw02-filter-button'} onClick={(props.filterUsers)}>SHOW ME FRIENDS FROM LA</button>
