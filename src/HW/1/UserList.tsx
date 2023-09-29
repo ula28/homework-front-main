4be940122eb1bfcc5bd66b33956dc7751db4ac85
ula28 <ula24499@gmail.com>
2023-09-30 00:28:32 +0200

    dom2

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,30 +1,37 @@
+
+
 type AddressType = {
   street: string; // ПОДПРАВЛЯЕМ any
   city: string; // ПОДПРАВЛЯЕМ any
 };
 
-type UserType = {
+export type UserType = {
   // ПРИДЕТСЯ САМОМУ)
-  users:[]
+  id:number
+  name:string
+  age:number
+  address:AddressType
 };
 
 type UserListPropsType = {
-  users: UserType; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+  users: UserType[];// чего-то не хв
+  
+
 };
 
-export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
+export const UserList = (props: UserListPropsType) => {
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
 
-      {/*<ul>*/}
-      {/*  {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...*/}
-      {/*    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>*/}
-      {/*      <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>*/}
-      {/*      {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
+      <ul>
+        {props.users.map((user) => ( 
+          <li key={ user.id} id={`hw01-user-${user.id}`}>
+           <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+            {user.address.street}, {user.address.city}
+         </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { UserList2 } from './UserList2';
+import { UserList2} from './UserList2';
 
 export type AddressType = {
   street: string;
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends:  UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -29,8 +29,8 @@ export const HW2 = () => {
 
   // ❗ Массив с данными не трогаем!
 
-  const users = {
-    myFriends:[
+  const users:UsersObjectType = {
+    myFriends :[
       { id: 1, name: 'John', age: 25, address: { street: '123 Main St', city: 'New York' } },
       { id: 2, name: 'Alice', age: 30, address: { street: '456 Elm St', city: 'San Francisco' } },
       { id: 3, name: 'Bob', age: 35, address: { street: '789 Oak St', city: 'Seattle' } },
@@ -47,7 +47,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter(user=>user.address.city==="Los Angeles")//'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
