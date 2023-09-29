5370268b722a1a192b4fef75e0af7c7530bf3118
ula28 <ula24499@gmail.com>
2023-09-30 01:03:47 +0200

    dd2

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -10,7 +10,7 @@ export type UserList2PropsType = {
 export const UserList2 = (props: UserList2PropsType) => {
 	return (
 		<div id={'hw02-users'}>
-			
+
 			<h2>User List:</h2>
 
 			<button id={'hw02-filter-button'} onClick={(props.filterUsers)}>SHOW ME FRIENDS FROM LA</button>
